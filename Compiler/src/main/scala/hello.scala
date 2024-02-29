94e6902617794ba95a2e7c31076ffb3562763d3a
Spencer Sullivan <spencer.sullivan@gmail.com>
2024-02-28 19:56:11 -0800

    more tokenizer nonsense.

--- Compiler/src/main/scala/hello.scala
+++ Compiler/src/main/scala/hello.scala
@@ -11,6 +11,10 @@ case object leftBracesToken extends Token
 case object leftParenToken extends Token
 case object rightParenToken extends Token
 case object rightBracesToken extends Token
+case object plusEqualsToken extends Token
+case object minusEqualsToken extends Token
+case object plusPlusToken extends Token
+case object minusMinusToken extends Token
 case object eventToken extends Token
 case object actorToken extends Token
 //case object boolToken extends Token
@@ -23,6 +27,7 @@ case object semicolonToken extends Token
 
 case class IdentifierToken(name: String) extends Token
 case class IntegerLiteralToken(value: Int) extends Token
+case class StringLiteralToken(value: String) extends Token
 
 object Tokenizer{
   /*@main def hello(): Unit = {
@@ -50,6 +55,8 @@ object Tokenizer{
   /*
   we could instead of splitting the input string, we could
   work through it as a list of characters, processing one character at a time.
+  This would allow us to handle multi-character tokens more easily.
+  The current setup probably won't work well for comments and string literals because we're stripping whitespace.
  */
   def test1tokenize(input:String): List[Token] = {
     // for yield is just shorthand for a map and a flatten
@@ -61,6 +68,10 @@ object Tokenizer{
       case "}" => rightBracesToken
       case "(" => leftParenToken
       case ")" => rightParenToken
+      case "+=" => plusEqualsToken
+      case "-=" => minusEqualsToken
+      case "++" => plusPlusToken
+      case "--" => minusMinusToken
       case "event" => eventToken
       case "actor" => actorToken
       case "statemachine" => statemachineToken
@@ -69,13 +80,18 @@ object Tokenizer{
       case "INTEGER" => INTEGERToken
       case "BOOL" => BOOLToken
       case ";" => semicolonToken
-      // need to account for += and -=, as well as ++ and --
 
       case _ =>
         if(token.matches("[a-zA-Z][a-zA-Z0-9]*")){
           IdentifierToken(token)
-        }else if(token.matches("[0-9]+")){
+        }else if(token.matches("[0-9]+")) {
           IntegerLiteralToken(token.toInt)
+        }else if(token.matches("//.*") || token.matches("/\\*.*\\*/")){
+          // ignore comments
+          None
+        // string literals
+        }else if(token.matches("\"[a-zA-Z0-9]*\"")){
+          StringLiteralToken(token)
         }else{
           throw new IllegalArgumentException(s"Unrecognized token: $token")
         }
