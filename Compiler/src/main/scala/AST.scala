4436ba7b5fadac14ae2115dd8d2ac620e9a6a4e0
morganceili <morganceili@gmail.com>
2024-04-06 14:36:44 -0700

    AST beginnings

--- Compiler/src/main/scala/AST.scala
+++ Compiler/src/main/scala/AST.scala
@@ -42,6 +42,44 @@ case object ExclusiveOrAssignmentOperator extends BinaryOperator
  *  EventName: NAME
  *  VarName: NAME
  * */
+
+//BinOp: '*' | '/' | '%' | '+' | '-' | '<<' | '>>' | '<' | '>' | '<=' | '>=' | '==' | 
+//'!=' | '^' | '&&' | '||' | '*=' | '/=' | '%=' | '+=' | '-=' | '<<=' | '>>=' | '^='
+sealed trait BinaryOperator 
+case object multiplyOperator
+case object divideOperator 
+case object moduloOperator 
+case object addOperator 
+case object subtractOperator 
+case object leftShiftOperator 
+case object rightShiftOperator 
+case object lessThanOperator 
+case object greaterThanOperator 
+case object lessThanOrEqualToOperator 
+case object greaterThanOrEqualToOperator 
+case object equalToOperator 
+case object notEqualToOperator 
+case object exclusiveOrOperator
+case object andOperator
+case object orOperator 
+case object multiplyAssignmentOperator 
+case object divideAssignmentOperator 
+case object moduloAssignmentOperator 
+case object addAssignmentOperator 
+case object subtractAssignmentOperator 
+case object leftShiftAssignmentOperator 
+case object leftShiftAssignmentOperator 
+case object exclusiveOrAssignmentOperator
+
+//Ask Vicki about this in the grammar
+sealed trait Expression
+case class IntLiteralExpression(value: Int) extends Expression
+case class StringLiteralExpression(value: String) extends Expression
+case class BoolLiteralExpression(value: Boolean) extends Expression
+case class BinaryOperationExpression(left: Expression, op: BinaryOperator, right: Expression) extends Expression
+
+//Type: 'int' | 'string' | 'bool' | 'actorname' | 'statename' | 'eventname'
+//Ask Dewey or Vicki about this - are actor/state/event names types rather than strings?
 sealed trait Type
 case object IntType extends Type
 case object StringType extends Type
@@ -184,3 +222,7 @@ case class ExprListCurly(expressions: Seq[Expression]) extends Expression
 case class PrintStmt(expressions: ExprListParen) extends Statement
 case class PrintlnStmt(expressions: ExprListParen) extends Statement
 case class ReturnStmt(expression: Expression) extends Statement
+case object ActorNameType extends Type
+case object StateNameType extends Type
+case object EventNameType extends Type
+case class HsmName(name: String)
--- Compiler/src/main/scala/Parser.scala
+++ Compiler/src/main/scala/Parser.scala
@@ -32,3 +32,31 @@ def token(expected: Token): Parser[Unit] = {
 @author Morgan Barrett
 Proeus V3 Parser
 */
+
+import AST
+import scala.util.parsing.combinator.Parsers
+import scala.util.parsing.combinator._
+
+/*
+Sequence (Seq[]) is used to represent indexed sequences that have a defined order of element i.e. guaranteed immutable. 
+The elements of sequences can be accessed using their indexes. 
+Sequences can also be accessed reversibly using the method reverse and reverseIterator
+*/
+case class TokenRead(tokens: Seq[Token]) extends Reader[Token]{
+
+}
+
+// Parser combinator that checks if the first token in the list matches the expected token. Consumes it if it does.
+// Returns none if the next token does not match the expected token 
+// => operator takes one type and returns another
+type Parser[A] = (List[Token]) => Option[(A, List[Token])]
+
+// def function accepts a parameter "expected" of type Token, returns value of type Parser[Unit]
+def token(expected: Token): Parser[Unit] = {
+  (tokens: List[Token]) => {
+     tokens match {
+      head :: tail if head == expected => Some(((), tail))
+      _ => None
+    }
+  }
+}
