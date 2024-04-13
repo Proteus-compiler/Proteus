65ca0fcebfa8b23a96bee115c48a747f5d3f61b6
morganceili <morganceili@gmail.com>
2024-04-12 17:00:50 -0700

    some AST work

--- Compiler/src/main/scala/AST.scala
+++ Compiler/src/main/scala/AST.scala
@@ -3,30 +3,30 @@
 //BinOp: '*' | '/' | '%' | '+' | '-' | '<<' | '>>' | '<' | '>' | '<=' | '>=' | '==' | 
 //'!=' | '^' | '&&' | '||' | '*=' | '/=' | '%=' | '+=' | '-=' | '<<=' | '>>=' | '^='
 sealed trait BinaryOperator 
-case object multiplyOperator
-case object divideOperator 
-case object moduloOperator 
-case object addOperator 
-case object subtractOperator 
-case object leftShiftOperator 
-case object rightShiftOperator 
-case object lessThanOperator 
-case object greaterThanOperator 
-case object lessThanOrEqualToOperator 
-case object greaterThanOrEqualToOperator 
-case object equalToOperator 
-case object notEqualToOperator 
-case object exclusiveOrOperator
-case object andOperator
-case object orOperator 
-case object multiplyAssignmentOperator 
-case object divideAssignmentOperator 
-case object moduloAssignmentOperator 
-case object addAssignmentOperator 
-case object subtractAssignmentOperator 
-case object leftShiftAssignmentOperator 
-case object leftShiftAssignmentOperator 
-case object exclusiveOrAssignmentOperator
+case object multiplyOperator extends BinaryOperator
+case object divideOperator extends BinaryOperator
+case object moduloOperator extends BinaryOperator
+case object addOperator extends BinaryOperator
+case object subtractOperator extends BinaryOperator
+case object leftShiftOperator extends BinaryOperator
+case object rightShiftOperator extends BinaryOperator
+case object lessThanOperator extends BinaryOperator
+case object greaterThanOperator extends BinaryOperator
+case object lessThanOrEqualToOperator extends BinaryOperator
+case object greaterThanOrEqualToOperator extends BinaryOperator
+case object equalToOperator extends BinaryOperator
+case object notEqualToOperator extends BinaryOperator
+case object exclusiveOrOperator extends BinaryOperator
+case object andOperator extends BinaryOperator
+case object orOperator extends BinaryOperator
+case object multiplyAssignmentOperator extends BinaryOperator
+case object divideAssignmentOperator extends BinaryOperator
+case object moduloAssignmentOperator extends BinaryOperator
+case object addAssignmentOperator extends BinaryOperator
+case object subtractAssignmentOperator extends BinaryOperator
+case object leftShiftAssignmentOperator extends BinaryOperator
+case object leftShiftAssignmentOperator extends BinaryOperator
+case object exclusiveOrAssignmentOperator extends BinaryOperator
 
 //Ask Vicki about this in the grammar
 sealed trait Expression
@@ -34,14 +34,42 @@ case class IntLiteralExpression(value: Int) extends Expression
 case class StringLiteralExpression(value: String) extends Expression
 case class BoolLiteralExpression(value: Boolean) extends Expression
 case class BinaryOperationExpression(left: Expression, op: BinaryOperator, right: Expression) extends Expression
+case class VarNameExpression(name: VarName) extends Expression
+case class ActorNameExpression(name: ActorName) extends Expression
+case class EventNameExpression(name: EventName) extends Expression
 
 //Type: 'int' | 'string' | 'bool' | 'actorname' | 'statename' | 'eventname'
-//Ask Dewey or Vicki about this - are actor/state/event names types rather than strings?
 sealed trait Type
 case object IntType extends Type
 case object StringType extends Type
 case object BoolType extends Type
-case object ActorNameType extends Type
+case object ActorNameType extends Type 
 case object StateNameType extends Type
 case object EventNameType extends Type
+
 case class HsmName(name: String)
+
+sealed trait DefActor
+case object actor extends Type
+//DefActor: 'actor' ActorName '{' ActorItem* '}'
+case class ActorName(ActorItems: Seq[ActorItems]) extends DefActor
+
+//Stmt: IfStmt | WhileStmt | DecStmt | AssignStmt | 
+//ExitStmt | ApplyStmt | SendStmt | PrintStmt | PrintlnStmt
+//IfStmt: 'if' ParenExpr Block ['else' (IfStmt | Block)] 
+sealed trait Statement
+case class IfStmt (guard: Expresssion, ifTrue: Statement, ifFalse: Optional[Statement]) extends Statement
+case object BlockStatement (items: Seq[Statement]) extends Statement
+case object WhileStmt (guard: Expression, ) extends Statement
+case object DecStmt () extends Statmenet
+case object AssignStmt () extends Statement
+case object ExitStmt () extends Statement
+case object ApplyStmt () extends Statement
+case object SendStmt () extends Statement
+case object PrintStmt () extends Statement
+case object PrintlnStmt () extends Statement
+
+sealed trait Block
+case class Block(item: Seq[Stmt]) extends Block //check Stmt vs Statement
+
+//def is a method, val is an instance variable
