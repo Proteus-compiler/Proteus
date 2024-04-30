f44bbf91cc2129c5e4762fc1a8913d4166bd93ca
Zucaritas46 <frosty46@protonmail.com>
2024-04-29 19:25:46 -0700

    Updated AST

--- Compiler/src/main/scala/AST.scala
+++ Compiler/src/main/scala/AST.scala
@@ -2,7 +2,6 @@
  *  @author Jorge Enriquez
  **/
 
-//TODO: HSMName is missing from the grammar
 
 /** =================  Binary Operators  =================
  * BinOp: '*' | '/' | '%' | '+' | '-' | '<<' | '>>' | '<' | '>' | '<=' | '>=' | '==' |
@@ -40,33 +39,35 @@ case object ExclusiveOrAssignmentOperator extends BinaryOperator
  *  ActorName: NAME
  *  StateName: NAME
  *  EventName: NAME
+ *  VarName: NAME
  * */
 sealed trait Type
 case object IntType extends Type
 case object StringType extends Type
 case object BoolType extends Type
-case object ActorNameType extends Type 
-case object StateNameType extends Type
-case object EventNameType extends Type
+case class ActorName(name: String) extends Type
+case object StateName(name: String) extends Type
+case object EventName(name: String) extends Type
+case object VarName(name: String) extends Type
+case object HSMName(name:String) extends Type
 
 /** Expr: ValExpr | BinOpExpr | ApplyExpr
  *  ValExpr: VarExpr | IntExpr | StrExpr | BoolExpr | ActorExpr | StateExpr | EventExpr | ParenExpr
- *  VarExpr: STRING (removed VarName)
- *  IntExpr: NUMBER 
+ *  VarExpr: VarName
+ *  IntExpr: NUMBER
  *  StrExpr: STRING 
  *  BoolExpr: BOOL
  *  ApplyExpr: FuncName ExprListParen
  *  ExprListParen :'(' [Expr (',' Expr)*] ')'
  *  BinOpExpr: ValExpr BinOp Expr
- *  ConstExpr: IntExpr | BoolExpr | StrExpr 
- *  // This needs clarifying
+ *  ConstExpr: IntExpr | BoolExpr | StrExpr
  *  ActorExpr: 'actor' ActorName
  *  StateExpr: 'state' StateName
  *  EventExpr: 'event' EventName
  *  ParenExpr: '(' Expr ')' 
  * */
 sealed trait Expression
-case class VarExpression(name: String) extends Expression // this would usually take in VarName which would hold a string
+case class VarExpression(name: VarName) extends Expression // redundant??
 case class IntLiteralExpression(value: Int) extends Expression
 case class StringLiteralExpression(value: String) extends Expression
 case class BoolLiteralExpression(value: Boolean) extends Expression
@@ -77,9 +78,9 @@ sealed trait ConstExpr extends Expression
 case class IntConstLiteralExpression(value: Int) extends ConstExpr
 case class StringConstLiteralExpression(value: String) extends ConstExpr
 case class BoolConstLiteralExpression(value: Boolean) extends ConstExpr
-case class ActorNameExpression(name: String) extends Expression
-case class StateNameExpression(name: String) extends Expression
-case class EventNameExpression(name: String) extends Expression
+case class ActorNameExpression(name: ActorName) extends Expression
+case class StateNameExpression(name: StateName) extends Expression
+case class EventNameExpression(name: EventName) extends Expression
 case class ParenExpression(innerExpression: Expression) extends Expression
 
 
@@ -93,34 +94,34 @@ case class ParenExpression(innerExpression: Expression) extends Expression
  * */
 case class Program(defEvents: Seq[DefEvent], defGlobalConsts: Seq[DefGlobalConst],
                    defFuncs: Seq[DefFunc], defActors: Seq[DefActor])
-case class DefEvent(eventName: String, types: Seq[Type]) //Must have at least one
-case class DefGlobalConst(constType: Type, varName: String, constExpr: Expression)
+case class DefEvent(name: EventName, types: Seq[Type]) //Must have at least one
+case class DefGlobalConst(constType: Type, varName: VarName, constExpr: ConstExpr)
 case class DefFunc(funcName: String, args: FormalFuncArgs, returnType: Option[Type], block: Block) extends ActorItem
 
 /** =================  Actors & their components =================
- *  DefActor: 'actor' ActorName '{' ActorItem* '}' //ActorName implemented as STRING
+ *  DefActor: 'actor' ActorName '{' ActorItem* '}'
  *  ActorItem: DefHSM | DefActorOn | DefMember | DefMethod
  *  DefHSM:   'statemachine' '{' StateItem* '}'
  *  DefActorOn: 'on' EventMatch OnBlock
- *  DefMember: Type VarName '=' ConstExpr ';'
- *  DefMethod: 'func' FuncName FormalFuncArgs ['->' Type] Block
+ *  DefMember: Type VarName '=' ConstExpr ';' //Different from StateItem
+ *  DefMethod: 'func' FuncName FormalFuncArgs ['->' Type] Block //Different from StateItem
  *  FormalFuncArgs : '(' [Type VarName (',' Type VarName)*] ')'
  * */
-case class DefActor(actorName: String, items: Seq[ActorItem]) //ActorName???
+case class DefActor(name: ActorName, items: Seq[ActorItem])
 sealed trait ActorItem
 case class DefHSM(stateItems: Seq[StateItem]) extends ActorItem
 case class DefActorOn(eventMatch: EventMatch, onBlock: Block) extends ActorItem
-case class DefMember(memberType: Type, varName: String, constExpr: Expression) extends ActorItem //Should this extend ActorItem or StateItem
-case class DefMethod(funcName: String, args: Seq[(Type, String)], returnType: Option[Type], block: Block) extends ActorItem //Should this extend ActorItem or StateItem
-case class FormalFuncArgs(args: Seq[(Type, String)])
+case class DefMember_ActorItem(memberType: Type, varName: VarName, constExpr: ConstExpr) extends ActorItem
+case class DefMethod_ActorItem(funcName: String, args: Seq[(Type, String)], returnType: Option[Type], block: Block) extends ActorItem
+case class FormalFuncArgs(args: Seq[(Type, VarName)])
 
 /** =================  States & transitions ================= 
  *  StateItem: DefOn | DefEntry | DefExit | DefMember | DefMethod | DefState | InitialState
  *  DefOn: 'on' EventMatch OnBody
  *  DefEntry: 'entry' '{' Block '}'
  *  DefExit: 'exit' '{' Block '}'
- *  DefMember: Type VarName '=' ConstExpr ';'                   **DEFINED in Actors and their components**
- *  DefMethod: 'func' FuncName FormalFuncArgs ['->' Type] Block **DEFINED in Actors and their components**
+ *  DefMember: Type VarName '=' ConstExpr ';'                   //Different from ActorItem
+ *  DefMethod: 'func' FuncName FormalFuncArgs ['->' Type] Block //Different from ActorItem
  *  DefState: 'state' StateName '{' StateItem* '}'
  *  InitialState: 'initial' StateName ';'
  *  
@@ -129,8 +130,11 @@ sealed trait StateItem
 case class DefOn(eventMatch: EventMatch, onBody: OnBody) extends StateItem
 case class DefEntry(block: Block) extends StateItem
 case class DefExit(block: Block) extends StateItem
-case class DefState(stateName: String, items: Seq[StateItem]) extends StateItem
-case class InitialState(stateName: String) extends StateItem
+case class DefState(name: StateName, items: Seq[StateItem]) extends StateItem
+case class DefMember_StateItem(memberType: Type, varName: VarName, constExpr: ConstExpr) extends StateItem
+case class DefMethod_StateItem(funcName: String, args: Seq[(Type, String)], returnType: Option[Type], block: Block) extends StateItem
+
+case class InitialState(name: StateName) extends StateItem
 
 /** =================  Matching events & actions ================= 
  *  EventMatch: EventName '{' [VarName (',' VarName)*] '}'
@@ -142,12 +146,12 @@ case class InitialState(stateName: String) extends StateItem
  *  OnBlock: Block
  * */
 
-case class EventMatch(eventName: String, varNames: Seq[String])
+case class EventMatch(name: EventName, varNames: Seq[VarName])
 sealed trait OnBody
 sealed trait GoStmt extends OnBody
-case class JustGoStmt(stateName: String, block: Block) extends GoStmt
-case class GoIfStmt(condition: Expression, stateName: String, thenBlock: Block, elseGo: Option[GoStmt]) extends GoStmt
-case class ElseGoStmt(stateName: String, block: Block) extends GoStmt
+case class JustGoStmt(name: StateName, block: Block) extends GoStmt
+case class GoIfStmt(condition: Expression, name: StateName, thenBlock: Block, elseGo: Option[GoStmt]) extends GoStmt
+case class ElseGoStmt(name: StateName, block: Block) extends GoStmt
 case class OnBlock(block: Block) extends OnBody
 
 
@@ -170,11 +174,11 @@ case class Block(statements: Seq[Statement])
 sealed trait Statement
 case class IfStmt (guard: Expression, ifTrueBlock: Statement, ifFalseBlock: Option[Block]) extends Statement
 case class WhileStmt (guard: Expression, body: Statement) extends Statement
-case class DecStmt (decType: Type, varName: String, expr: Expression) extends Statement
-case class AssignStmt(varName: String, expr: Expression) extends Statement
+case class DecStmt (decType: Type, varName: VarName, expr: Expression) extends Statement
+case class AssignStmt(varName: VarName, expr: Expression) extends Statement
 case class ExitStmt (num: Int) extends Statement
 case class ApplyStmt(expression: ApplyExpression) extends Statement
-case class SendStmt(hsmName: String, eventName: String, expressions: ExprListCurly) extends Statement //HSMName is not defined in the Grammar
+case class SendStmt(hsmName: HSMName, eventName: EventName, expressions: ExprListCurly) extends Statement //HSMName is not defined in the Grammar
 case class ExprListCurly(expressions: Seq[Expression]) extends Expression
 case class PrintStmt(expressions: ExprListParen) extends Statement
 case class PrintlnStmt(expressions: ExprListParen) extends Statement
